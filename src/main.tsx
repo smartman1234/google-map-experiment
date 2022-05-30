b163af4a61a9b85bdc08031ed1691f3fd326772b
smartman1234 <john.smith.dev0724@gmail.com>
2022-05-30 21:35:40 +0800

    add possible solution for amplify build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "antd/dist/antd.css";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import resolve from "@rollup/plugin-node-resolve";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      {
+        ...resolve({
+          preferBuiltins: false,
+          browser: true,
+        }),
+        enforce: "pre",
+        apply: "build",
+      },
+      visualizer(),
+    ],
     define: {
       "process.env.GOOGLE_API_KEY": JSON.stringify(process.env.GOOGLE_API_KEY),
       "process.env.GOOGLE_SEARCH_URL": JSON.stringify(
@@ -21,11 +34,6 @@ export default ({ mode }) => {
       "process.env.TIME_ZONE_ACCESS_KEY": JSON.stringify(
         process.env.TIME_ZONE_ACCESS_KEY
       ),
-    },
-    resolve: {
-      alias: {
-        "./runtimeConfig": "./runtimeConfig.browser",
-      },
     },
   });
 };
